e05d78074e7b8ddca2cb38c6d5fd88845c47f1f4
hwj <wjhuang@sjtu.edu.cn>
2022-03-24 11:46:54 +0800

    fix problem activate python env in recv.sh
    Signed-off-by: hwj <wjhuang@sjtu.edu.cn>

--- recv.sh
+++ recv.sh
@@ -1,5 +1,7 @@
-rm result/*
+rm result/* &&
 
-export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
+conda activate alphartc &&
+
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH &&
 
 python3 ./peerconnection_serverless.py ./receiver_pyinfer.json
