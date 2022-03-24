f17e103df2184208a47922af74a3ebb5042500d3
hwj <wjhuang@sjtu.edu.cn>
2022-03-24 11:40:07 +0800

    add shell scripts to run peerconnection demo

---
+++ recv.sh
@@ -0,0 +1,5 @@
+rm result/*
+
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
+
+python3 ./peerconnection_serverless.py ./receiver_pyinfer.json
---
+++ send.sh
@@ -0,0 +1,5 @@
+conda activate alphartc &&
+
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH &&
+
+python3 ./peerconnection_serverless.py ./sender_pyinfer.json
